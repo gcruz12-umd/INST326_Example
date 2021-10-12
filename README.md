a73aa8aeecaa6010ade49f074c96ed4c2e49acfc
Gabriel Cruz <gcruz12@terpmail.umd.edu>
2021-10-12 10:11:09 -0400

    Ive made some changes

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # INST326_Example
 This is an example repo
+
+## This is a second heading
+This heading has some more information about what this repo is.
---
+++ cars/make_cars.py
@@ -0,0 +1,10 @@
+"""Quick Docstring"""
+
+class Car:
+    def __init__(self):
+        self.x = 0
+        self.y = 0
+        
+        
+if __name__ == '__main__':
+    Car()
